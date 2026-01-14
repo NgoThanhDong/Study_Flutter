b3a29ab40afedcf3f786c5be223aaeadba1785a2
dongnt-pasona <n-thanhdong@pasona.vn>
2026-01-14 11:40:28 +0700

    Take your Flutter app from boring to beautiful
    Step 5

--- boring_to_beautiful/lib/src/shared/views/outlined_card.dart
+++ boring_to_beautiful/lib/src/shared/views/outlined_card.dart
@@ -21,7 +21,15 @@ class _OutlinedCardState extends State<OutlinedCard> {
       cursor: widget.clickable
           ? SystemMouseCursors.click
           : SystemMouseCursors.basic,
-      child: Container(child: widget.child),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Theme.of(context).colorScheme.outline,
+            width: 1,
+          ),
+        ),
+        child: widget.child,
+      ),
     );
   }
 }
